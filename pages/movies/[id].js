5a95f27c2b9eb677b3084aae10d7eccc6e4bf2d9
Vinaypatil-Ev <vinaypatil153@gmail.com>
2021-06-07 20:18:39 +0530

    07 June 2021

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -15,7 +15,7 @@ export default function movie(props) {
 export async function getServerSideProps({params}) {
     // const res = await getDataByQuery(params.id)
     const rmovies = getRMovie()
-    const res = "null"
+    const res = "null" 
     return {
         props: {
             movies: res,
