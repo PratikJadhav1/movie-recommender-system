25b4665aab6df7f834a71130782635750aa341dc
Vinaypatil-Ev <vinaypatil153@gmail.com>
2021-07-01 14:44:27 +0530

    01 June 2021

--- components/NavBarC.jsx
+++ components/NavBarC.jsx
@@ -5,14 +5,7 @@ export default class Navbar extends Component {
     render() {
         return (
             <div>
-                <div className={styles.header}>
-                <div >Home</div>
-                <div>genres</div>
-                <div>sing up</div>
-                <div>Login</div>
-                </div>
                 <div>{this.props.children}</div>
-                <div className={styles.footer}>Footer</div>
             </div>
         )
     }
